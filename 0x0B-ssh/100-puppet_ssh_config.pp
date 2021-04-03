f079ce1c8b56dd1f516c519106a02b43c7c619a7
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-03 10:40:46 +0000

    try fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # Setup a SSH client configuration file to connect to a server with RSA key.
 
 file_line { 'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+  path    => '/etc/ssh/ssh_config',
+  match   => '.+PasswordAuthentication.+'
+  line    => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true
 }
