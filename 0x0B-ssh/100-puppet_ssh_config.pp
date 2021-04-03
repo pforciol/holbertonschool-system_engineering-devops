597f914af98b51df970e110ad3272b6b896e4bfe
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-03 10:34:08 +0000

    try fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 
 file_line { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication yes',
+  line => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
