316fc0c564659a416c54f8bcdf8c8eb27f8bdd77
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-06 10:01:25 +0000

    Fix 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,7 +16,7 @@ file { '/var/www/html/index.html':
 file_line { 'Rick Astley showtime':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'listen [::]:80 default_server;',
+  after  => 'listen 80 default_server;',
   line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
 }
 
