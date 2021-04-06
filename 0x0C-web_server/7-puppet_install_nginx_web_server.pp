caba97a9b96ea9668dd5d6bc53f0105813133785
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-06 09:56:24 +0000

    Fix 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,12 +1,13 @@
 # Script that setup a nginx web server on our server + redirection.
 
 package { 'nginx':
-  ensure   => installed,
+  ensure   => present,
   provider => 'apt'
 }
 
 # Index page
 file { '/var/www/html/index.html':
+  ensure  => present,
   path    => '/var/www/html/index.html',
   content => 'Holberton School'
 }
@@ -14,13 +15,15 @@ file { '/var/www/html/index.html':
 # Redirect to fabulous Rick Astley page
 file_line { 'Rick Astley showtime':
   ensure => 'present',
-  path => '/etc/nginx/sites-available/default',
-  after => 'listen [::]:80 default_server;',
-  line => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen [::]:80 default_server;',
+  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
 }
 
 service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-  subscribe => File_line['redirect_me']
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['redirect_me']
 }
