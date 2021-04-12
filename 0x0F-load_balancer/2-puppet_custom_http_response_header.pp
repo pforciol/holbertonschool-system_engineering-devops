9b13ca7677ef8ec4801be19f54b0f6e21d0cf82a
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-12 18:47:17 +0000

    Add task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,40 +1,38 @@
 # Script that setup a nginx web server on our server + redirection + header
 exec { 'update':
-  command => '/usr/bin/apt update'
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
   ensure  => installed,
-  require => Exec['update']
+  name    => 'nginx',
+  require => Exec['update'],
 }
 
-# Index page
 file { '/var/www/html/index.html':
-  ensure  => present,
+  ensure  => 'present',
   path    => '/var/www/html/index.html',
   content => 'Holberton School',
-  require => Package['nginx']
+  require => Package['nginx'],
 }
 
-# Redirect to fabulous Rick Astley page
-file_line { 'Rick Astley showtime':
+file_line { 'redirect_me':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
-  require => Package['nginx']
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
-# Redirect to fabulous Rick Astley page
-file_line { 'X-Server-By header addition':
+file_line { 'addHeader':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => '        add_header X-Server-By $hostname;',
-  require => Package['nginx']
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
   ensure  => running,
-  require => Package['nginx']
+  require => Package['nginx'],
 }
